211e9a6db4830093661eaf6f625442b84589b2bb
monadyn <monad@126.com>
2016-02-11 20:18:07 -0600

    a

--- rubbos_init/build_image.sh
+++ rubbos_init/build_image.sh
@@ -1,3 +1,5 @@
+docker login
+
 ip=$(curl -X GET  http://192.168.0.1:8080/v2/apps/mysql3319/tasks -H "Content-type: application/json" | python -c 'import sys, json; print json.load(sys.stdin)["tasks"][0]["host"];')
 port=$(curl -X GET  http://192.168.0.1:8080/v2/apps/mysql3319/tasks -H "Content-type: application/json" | python -c 'import sys, json; print json.load(sys.stdin)["tasks"][0]["ports"][0];')
 sed "s/RUBBOS_DB_URL/${ip}:${port}/g" mysql.properties.template > mysql.properties
@@ -7,4 +9,4 @@ echo $port
 docker rmi hudsonshan/rubbos_tomcat:app_dyn
 docker build -t hudsonshan/rubbos_tomcat:app_dyn .
 
-
+docker push hudsonshan/rubbos_tomcat:app_dyn
